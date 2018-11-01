fc7c1243fc436663755bc1e7dcf5f55def876c30
ktochow1 <ktochow1@binghamton.edu>
2018-11-01 16:12:31 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,6 +7,7 @@ class Shoe
   def initialize(brand)
     @brand = brand
     #BRANDS << @brand
+    BRANDS << brand unless BRANDS.include?(brand)
       if !BRANDS.include?(@brand)
         BRANDS << @brand
     end
